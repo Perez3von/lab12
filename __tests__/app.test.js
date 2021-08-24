58bdc684dcbbac63313e5de73467d243a36c49f5
Evon Perez <perez.evon@gmail.com>
2021-08-24 17:19:05 -0400

    working get

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,9 +15,9 @@ describe('app routes', () => {
   
       await client.connect();
       const signInData = await fakeRequest(app)
-        .post('/auth/signup')
+        .post('/auth/signin')
         .send({
-          email: 'jon@user.com',
+          email: 'john@arbuckle.com',
           password: '1234'
         });
       
@@ -30,55 +30,26 @@ describe('app routes', () => {
 
     test('returns todos', async() => {
 
-      const expectation = [
+      const expectation = 
         {
           id:1,
           completed:false, 
           todo:'avoid dishes',
           user_id: 1
-        },
-        {
-          id:2,
-          completed:false, 
-          todo:'skip gym',
-          user_id: 1
-        },
-        {
-          id:3,
-          completed:false,
-          todo:'not take shower',
-          user_id: 1
-        }
-      ];
+        };
       
 
       const data = await fakeRequest(app)
-        .get('/todos')
-        .expect('Content-Type', /json/)
-        .expect(200);
+        .get('/api/todos')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/);
+        //.expect(200);
 
-      expect(data.body).toEqual(expectation);
+      expect(data.body[0]).toEqual(expectation);
     });
 
 
-    test('returns todo by id', async() => {
-
-      const expectation = {
-        id:1,
-        completed:false, 
-        todo:'avoid dishes',
-        user_id: 1
-      };
-      
-
-      const data = await fakeRequest(app)
-        .get('/todos/1')
-        .expect('Content-Type', /json/)
-        .expect(200);
-
-      expect(data.body).toEqual(expectation);
-    });
-
+   
 
 
 
