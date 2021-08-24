c283b07f26b1fb92eb6f05e7129aa60352a14e23
Evon Perez <perez.evon@gmail.com>
2021-08-23 20:17:23 -0400

    working get,post,put

---
+++ data/todos.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    
+    completed:false, 
+    todo:'avoid dishes',
+    user_id: 1
+  },
+  {
+  
+    completed:false, 
+    todo:'skip gym',
+    user_id: 1
+  },
+  {
+    
+    completed:false,
+    todo:'not take shower',
+    user_id: 2
+  }
+];
--- data/users.js
+++ data/users.js
@@ -2,5 +2,17 @@ module.exports = [
   {
     email: 'john@arbuckle.com',
     password: '1234'
+  },
+  {
+    email: 'kevin@bacon.com',
+    password: '4321'
+  },
+  {
+    email: 'captain@crunch.com',
+    password: '2468'
+  },
+  {
+    email: 'britney@spears.com',
+    password: '1357'
   }
 ];
